a613158811d3e8e5f2cb159a3b753179158959a7
Ramiz Ali <ramiz.apk7@gmail.com>
2023-02-22 02:35:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function fetchBooks() {
+ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) =>resp.json())
+  .then((json) => renderBooks(json));
   
+
 }
 
 function renderBooks(books) {
@@ -12,6 +16,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded',function() {
   fetchBooks();
 });
